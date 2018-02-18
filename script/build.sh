2c08e17870479ddfe66ada32a973fa16b33ddc67
m4kvn <fun.masahiro@gmail.com>
2018-02-18 23:51:44 +0900

    Update hugo build version

--- script/build.sh
+++ script/build.sh
@@ -8,7 +8,7 @@ public_dir=./public
 if [ ! -e ${hugo_bin} ]; then
     mkdir ${hugo_dir} ; pushd ${hugo_dir}
     hugo_tar=hugo.tar.gz
-    curl -sL https://github.com/gohugoio/hugo/releases/download/v0.30.2/hugo_0.30.2_Linux-64bit.tar.gz -o ${hugo_tar}
+    curl -sL https://github.com/gohugoio/hugo/releases/download/v0.31/hugo_0.31_Linux-64bit.tar.gz -o ${hugo_tar}
     tar zxvf ${hugo_tar}
     popd
 fi
